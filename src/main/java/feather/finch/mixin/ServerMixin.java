cf5af4ba354f37fc43f7536fc9a3c78d4da53ae3
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-25 21:48:07 +0500

    Update ServerMixin.java

--- src/main/java/feather/finch/mixin/ServerMixin.java
+++ src/main/java/feather/finch/mixin/ServerMixin.java
@@ -8,13 +8,31 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Mixin(MinecraftServer.class)
 public class ServerMixin {
     private static final Logger LOGGER = LoggerFactory.getLogger("FeatherFinch");
+    private static final ScheduledExecutorService CLEANER_SERVICE = Executors.newSingleThreadScheduledExecutor();
+    private static boolean isRunning = false;
 
     @Inject(at = @At("HEAD"), method = "loadLevel")
     private void featherfinch$onLoadLevel(CallbackInfo info) {
-        LOGGER.info("FeatherFinch: Очистка памяти перед загрузкой мира...");
-        System.gc(); 
+        if (!isRunning) {
+            LOGGER.info("FeatherFinch: Система мягкой очистки RAM запущена!");
+            
+            // Первая очистка перед тяжелой загрузкой
+            System.gc();
+
+            // Запускаем фоновую задачу: чистить память каждые 60 секунд
+            CLEANER_SERVICE.scheduleAtFixedRate(() -> {
+                // LOGGER.info("FeatherFinch: Фоновая уборка мусора..."); 
+                System.gc();
+            }, 1, 1, TimeUnit.MINUTES);
+
+            isRunning = true;
+        }
     }
 }
