53322815ba48470e7a9a10ec4344b119ec0f896c
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-25 21:17:24 +0500

    Create ServerMixin.java

---
+++ src/main/java/feather/finch/mixin/ServerMixin.java
@@ -0,0 +1,20 @@
+package feather.finch.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class ServerMixin {
+    private static final Logger LOGGER = LoggerFactory.getLogger("FeatherFinch");
+
+    @Inject(at = @At("HEAD"), method = "loadLevel")
+    private void featherfinch$onLoadLevel(CallbackInfo info) {
+        LOGGER.info("FeatherFinch: Очистка RAM перед загрузкой...");
+        System.gc(); 
+    }
+}
