bb9d99a072e446b00b01efa72b0c1383e42e214c
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-25 20:40:30 +0500

    Update ExampleMixin.java

--- src/main/java/feather/finch/mixin/ExampleMixin.java
+++ src/main/java/feather/finch/mixin/ExampleMixin.java
@@ -1,6 +1,8 @@
 package feather.finch.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,8 +10,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger("FeatherFinch");
+
+    @Inject(at = @At("HEAD"), method = "loadLevel")
+    private void featherfinch$onLoadLevel(CallbackInfo info) {
+        LOGGER.info("FeatherFinch: Расправляем крылья... Оптимизация загрузки мира запущена!");
+        
+        // Маленький хак: принудительный вызов очистки мусора перед тяжелой загрузкой мира
+        System.gc(); 
+        
+        long maxMemory = Runtime.getRuntime().maxMemory() / 1024 / 1024;
+        LOGGER.info("FeatherFinch: Доступно памяти: {} MB. Погнали!", maxMemory);
+    }
 }
